78bb7ea2c70e69824164956bf05bfff57d848b21
Adrián Álvarez Bombín <adrialvarezbom@gmail.com>
2026-03-11 11:18:57 +0100

    Add files via upload

---
+++ Teoria/UD1. Programación Multi-Proceso/UT1_01_EjecProceso_AULA/UT1_01_EjecProceso_AULA/src/psp/procesos/sumaFicheros/GeneraFicheroConNumeros.java
@@ -0,0 +1,31 @@
+package psp.procesos.sumaFicheros;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class GeneraFicheroConNumeros {
+    final static int NUM_LINEAS = 10000000;
+    final static int PESO = Integer.MAX_VALUE / 100;
+
+    public static void main(String[] args) {
+        if (args.length<1) {
+            System.out.println("Se necesita el nombre del fichero");
+            System.exit(1);
+        }
+
+        try{
+            PrintWriter salida= new PrintWriter(new FileWriter(args[0]));
+
+            for (int num=1; num <= NUM_LINEAS; num++) {
+                long aleatorio= (long)(Math.random() * PESO);
+                salida.println(aleatorio);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("Fichero generado");
+    }
+
+}
